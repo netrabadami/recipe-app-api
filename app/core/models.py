f44dabe4cf759bc6d4f04b76fc0f44f6c296ec67
netra badami <netracbadami@gmail.com>
2020-07-02 18:20:10 -0700

    Followed PEP8 guidlines

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
-# Create your models here.
+
 
 class UserManager(BaseUserManager):
 
@@ -23,8 +23,10 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-    
+
+
 class User(AbstractBaseUser, PermissionsMixin):
+
     """Custome user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
@@ -34,5 +36,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-
-    
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,14 +4,13 @@ from django.contrib.auth import get_user_model
 
 class ModelTest(TestCase):
 
-
     def test_create_user_with_email(self):
         """tests user creation with email"""
         email = 'test@django.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
